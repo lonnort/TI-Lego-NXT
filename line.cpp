3f6d0c330bca4806d8edaf3205709e2256c65a13
Lennard Duinkerken <lennarduinkerken@gmail.com>
2019-03-28 13:35:59 +0100

    Update

--- line.cpp
+++ line.cpp
@@ -8,7 +8,7 @@ BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
-int motorPower = 50;
+int motorPower = 30;
 
 void fwd(void) {
 	BP.set_motor_power(PORT_B, motorPower);
@@ -37,6 +37,7 @@ int main () {
 		error = 0;
 
 		if (BP.get_sensor(PORT_1, Light3) == 0) {
+			cou << "Light: " << Light3.reflected << endl;
 			if (Light3.reflected < 2250) {
 		 		BP.set_motor_power(PORT_C, motorPower/3);
 		 		sleep(1);
@@ -48,6 +49,7 @@ int main () {
 		}
 
 		else if (BP.get_sensor(PORT_2, Color1) == 0) {
+			cou << "Color: " << Color1.color << endl;
 			if ((int)Color1.color != 1) {
 				BP.set_motor_power(PORT_B, motorPower/3);
 		 		sleep(1);
