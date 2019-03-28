6d9aafc743736267ac7ef5a657498647f08f6cb0
Lennard Duinkerken <lennard@lennard-laptop.wifi.hu.nl>
2019-03-28 13:26:30 +0100

    Update

--- line.cpp
+++ line.cpp
@@ -26,26 +26,26 @@ int main () {
 	int error;
 
 	BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_COLOR_FULL);
-	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
+	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_LIGHT_ON);
 //	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_ULTRASONIC);
 
 	sensor_color_t		Color1;
-	sensor_ultrasonic_t	Ultrasonic2;
+	// sensor_ultrasonic_t	Ultrasonic2;
 	sensor_light_t		Light3;
 
 	while(true) {
 		error = 0;
 
-		if ((BP.get_sensor(PORT_3, Light3) == 0) && (BP.get_sensor(PORT_2, Color1) == 0) /*&& (BP.get_sensor(PORT_1, Ultrasonic2) == 0)*/) {
+		if ((BP.get_sensor(PORT_1, Light3) == 0) && (BP.get_sensor(PORT_2, Color1) == 0) /*&& (BP.get_sensor(PORT_1, Ultrasonic2) == 0)*/) {
 			cout << "Color: " << (int)Color1.color << " Light: " << Light3.reflected << endl;
 		 	if ((int)Color1.color != 1) {
-				BP.set_motor_power(PORT_C, motorPower/3);
+				BP.set_motor_power(PORT_B, motorPower/3);
 		 		sleep(1);
 		 		fwd();
 		 	}
 
 		 	else if (Light3.reflected < 2250) {
-		 		BP.set_motor_power(PORT_B, motorPower/3);
+		 		BP.set_motor_power(PORT_C, motorPower/3);
 		 		sleep(1);
 		 		fwd();
 		 	}
