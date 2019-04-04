bbf39211298e6b7ed31a049aa7e561a2a894d848
Lennard Duinkerken <lennarduinkerken@gmail.com>
2019-04-04 09:38:02 +0200

    update

--- batteryIndicator.cpp
+++ batteryIndicator.cpp
@@ -1,80 +1,16 @@
 #include <iostream>
-#include <csignal>
+#include <fstream>
 #include "BrickPi3.h"
 
 bool RUNNING = true;
 BrickPi3 BP;
 
-void exit_handler(int signal) {
-	std::cout << "CTRL-C: Exiting\n";
-	RUNNING = false;
-}
-
-int OUTPUT = 1;
-int INPUT  = 0;
-
-int HIGH   = 1;
-int LOW    = 0;
-
-void pinMode(int pin, int mode) {
-	if (mode == 1) {
-		command = "gpio mode " + (char)pin + " OUTPUT";
-		system(command);
-	}
-	if (mode == 0) {
-		command = "gpio mode " + (char)pin + " INPUT";
-		system(command);
-	}
-}
-
-void digitalWrite(int pin, int val) {
-	command = "gpio " + (char)pin + (char)val;
-	system("gpio %i %i", pin, val);
-}
-
 int main() {
-	std::signal(SIGINT, exit_handler);
-
-	int r = 0, y = 2, g = 4;
-
-	pinMode(r, OUTPUT);
-	pinMode(y, OUTPUT);
-	pinMode(g, OUTPUT);
-
-	while(RUNNING) {
-		V = BP.get_voltage_battery();
-
-		// V tussen 10 en 12 Volt
-		// r   :         V < 10
-		// r,y : 10   <= V < 10.5
-		// y   : 10.5 <= V < 11
-		// y,g : 11   <= V < 11.5
-		// g   : 11.5 <= V
-
-		if (V < 10.0) {
-			digitalWrite(r, HIGH);
-			digitalWrite(y, LOW);
-			digitalWrite(g, LOW);
-		}
-		else if (10.0 <= V || V < 10.5) {
-			digitalWrite(r, HIGH);
-			digitalWrite(y, HIGH);
-			digitalWrite(g, LOW);
-		}
-		else if (10.5 <= V || V < 11.0) {
-			digitalWrite(r, LOW);
-			digitalWrite(y, HIGH);
-			digitalWrite(g, LOW);
-		}
-		else if (11.0 <= V || V < 11.5) {
-			digitalWrite(r, LOW);
-			digitalWrite(y, HIGH);
-			digitalWrite(g, HIGH);
-		}
-		else if (11.5 <= V) {
-			digitalWrite(r, LOW);
-			digitalWrite(y, LOW);
-			digitalWrite(g, HIGH);
-		}
+	while(true) {
+		std::ofstream file;
+		file.open("batteryVoltage");
+		file << BP.get_voltage_battery();
+		file.close();
+		sleep(5);
 	}
 }
---
+++ batteryIndicator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from gpiozero import LED
+from time import sleep
+
+red    = LED(17)
+yellow = LED(27)
+green  = LED(23)
+
+while(True):
+	with open("batteryVoltage") as file:
+		voltage = file.read();
+
+	if voltage < 10:
+		red.on()
+		yellow.off()
+		green.off()
+	elif 10 <= voltage < 10.5:
+		red.on()
+		yellow.on()
+		green.off()
+	elif 10.5 <= voltage < 11:
+		red.off()
+		yellow.on()
+		green.off()
+	elif 11 <= voltage < 11.5:
+		red.off()
+		yellow.on()
+		green.on()
+	elif 11.5 <= voltage:
+		red.off()
+		yellow.off()
+		green.on()
+
+	sleep(5)
