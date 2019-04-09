1fdb2fd4eeabd308a4d85be0adeba8f502f2bc25
Lennard Duinkerken <lennarduinkerken@gmail.com>
2019-04-09 09:31:09 +0000

    update

---
+++ gertshead.hpp
@@ -0,0 +1,11 @@
+#ifndef GERTSHEAD_HPP
+#define GERTSHEAD_HPP
+#include "BrickPi3.h"
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+BrickPi3 BP;
+
+void armmotor(int angle);
+
+#endif /* GERTSHEAD_HPP */
